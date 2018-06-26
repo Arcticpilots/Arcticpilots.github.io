2f9e41d2dc7ccf0d69b6ee7938d94522d0313827
Arcticpilots <36964804+Arcticpilots@users.noreply.github.com>
2018-06-26 18:52:25 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -4,45 +4,45 @@
 // When size is submitted by the user, call makeGrid()
 
 $(document).ready(function (){
-		var color;
-		color = $("#colorPicker").val();
-		$("#colorPicker").change(function(){
-				color = $(this).val() ;
-		});
+	var color;
+	color = $("#colorPicker").val();
+	$("#colorPicker").change(function(){
+		color = $(this).val() ;
+	});
 
-$("#sizePicker").submit(makeGrid);
-//prevent the form from reloading 	after submit
-$("#sizePicker").submit(function(e) {
-    e.preventDefault();
-});
+	$("#sizePicker").submit(makeGrid);
+	//prevent the form from reloading after submit
+	$("#sizePicker").submit(function(e) {
+		e.preventDefault();
+	});
 
-function makeGrid() {
+	function makeGrid() {
 
-	if($('tbody').length > 0){
+		if($('tbody').length > 0){
 			$("tbody").remove();
-	}
+		}
 	
-	var N = $("#inputHeight").val();   //Height of Grid
-	var M= $("#inputWeight").val();  //Weight of Grid
-	var table =  $('table');
-	table.append('<tbody></tbody>');
-	$("tbody").append(createTable(N,M));
+		var N = $("#inputHeight").val();   //Height of Grid
+		var M= $("#inputWeight").val();  //Width of Grid
+		var table =  $('table');
+		table.append('<tbody></tbody>');
+		$("tbody").append(createTable(N,M));
 	
-	function createTable(line,col) { 
+		function createTable(line,col) { 
 			T="";
 			for(i=0;i<line;i++){
-					var T =T+"<tr>";
-					for(j=0;j<col;j++){
-							T +="<td></td>";
-					}
-					T +="</tr>";
+				var T =T+"<tr>";
+				for(j=0;j<col;j++){
+					T +="<td></td>";
+				}
+				T +="</tr>";
 			}
 			return T;
-	}
+		}
 
-}
- $('table').on('click', 'td', function(event) {
-        $( this ).css('backgroundColor', color);
-});
+	}
+	$('table').on('click', 'td', function(event) {
+		$( this ).css('backgroundColor', color);
+	});
 	
 });
